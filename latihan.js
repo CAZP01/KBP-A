13823da02d25e5c972a76708aadceb2aba663c75
Einseele <chairulazp@gmail.com>
2024-11-28 13:07:36 +0700

    Update

--- latihan.js
+++
@@ -1,5 +0,0 @@
-const filterGenap = (x) => {
-    return x.filter(num => num % 2 === 0);
-};
-
-console.log(filterGenap([1,2,3,4,5,6]));
--- latihan2.js
+++
@@ -1,8 +0,0 @@
-function alert (kalimat){
-    console.log(kalimat);
-}
-
-var bar = true;
-var foo = bar && alert("Hello Indonesia");
-var bar = false;
-var foo = bar && alert("Hello Indonesia");
